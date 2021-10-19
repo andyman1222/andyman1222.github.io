aac6182eff5132a403168ef86cd0b50c4bed055a
Andy Herbert <andyman1222@users.noreply.github.com>
2021-10-19 03:32:38 -0400

    Update engine.js

--- engine/engine/engine.js
+++ engine/engine/engine.js
@@ -53,7 +53,7 @@ function initDefaultGraphics(vertexPath, fragmentPath) {
 	coords = new object({ pos: vec3(0, 0, 0), rot: eulerToQuat(vec3(1, 0, 0), 0), scl: vec3(1, 1, 1) }, [{
 		pointIndex: [0, 1, 2, 3, 4, 5], matIndex: [0, 0, 1, 1, 2, 2], texCoords: [vec2(0,0), vec2(1,1), vec2(0,0), vec2(1,1), vec2(0,0), vec2(1,1)], type: gl.LINES}],
 	[vec3(-1000000, 0, 0), vec3(1000000, 0, 0), vec3(0, -1000000, 0), vec3(0, 1000000, 0), vec3(0, 0, -1000000), vec3(0, 0, 1000000)],
-	[vec4(1, 0, 0, 1), vec4(0, 1, 0, 1), vec4(0, 0, 1, 1)], [solidColorNoLighting(vec4(1,0,0,1)), solidColorNoLighting(vec4(0,1,0,1)), solidColorNoLighting(vec4(0,0,1,1))], "rect", true)
+	[vec4(1, 0, 0, 1), vec4(0, 1, 0, 1), vec4(0, 0, 1, 1)], [new solidColorNoLighting(vec4(1,0,0,1)), new solidColorNoLighting(vec4(0,1,0,1)), new solidColorNoLighting(vec4(0,0,1,1))], "rect", true)
 }
 
 function engineInit(userInit, userTick, defaultVertex = "../default-shaders/vertex.glsl", defaultFragment = "../default-shaders/fragment.glsl") {
