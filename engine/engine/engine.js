0d1961e2dcd4c7aee7830aa2c991c265c106dd71
Andy Herbert <andyman1222@users.noreply.github.com>
2021-10-19 23:01:55 -0400

    add light

--- engine/engine/engine.js
+++ engine/engine/engine.js
@@ -49,7 +49,7 @@ function initDefaultGraphics(vertexPath, fragmentPath) {
 	bData = new buffer(gl, program, "coordinates", "inMatProp1", "inMatProp2", "inMatProp3", "inMatProp4", "inMatIndex", "projMatrix", "viewMatrix", "normalMatrix", "lights", "maxLightIndex");
 
 	mainCamera = new camera(bData);
-
+	
 	coords = new object({ pos: vec3(0, 0, 0), rot: eulerToQuat(vec3(1, 0, 0), 0), scl: vec3(1, 1, 1) }, [{
 		pointIndex: [0, 1, 2, 3, 4, 5], matIndex: [0, 0, 1, 1, 2, 2], texCoords: [vec2(0,0), vec2(1,1), vec2(0,0), vec2(1,1), vec2(0,0), vec2(1,1)], type: gl.LINES}],
 	[vec3(-1000000, 0, 0), vec3(1000000, 0, 0), vec3(0, -1000000, 0), vec3(0, 1000000, 0), vec3(0, 0, -1000000), vec3(0, 0, 1000000)],
