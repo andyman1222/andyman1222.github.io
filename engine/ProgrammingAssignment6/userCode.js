b7b1ea192459457ea1eb92f5e8bc8295f47728c5
Andy Herbert <andyman1222@users.noreply.github.com>
2021-10-21 04:31:43 -0400

    Update userCode.js

--- engine/ProgrammingAssignment6/userCode.js
+++ engine/ProgrammingAssignment6/userCode.js
@@ -165,7 +165,8 @@ function init() {
 	mainCamera.transform.pos = vec3(-20, 2, -20)
 	new ambientLight(vec4(.1,.1,.1,1), null)
 	new directionalLight({pos: vec3(0,0,0), rot: eulerToQuat(vec3(-.5,-.5,-.5),0), scl: vec3(1,1,1)}, vec4(1,1,1,1), null)
-	mainCamera.attachChildToSelf(new pointLight({pos: vec3(0,0,0), rot: eulerToQuat(vec3(1,0,0),0),scl: vec3(1,1,1)}, vec4(.5,.5,0,1), null, 10), "relative")
+	var playerLight = new pointLight({pos: vec3(0,0,0), rot: eulerToQuat(vec3(1,0,0),0),scl: vec3(1,1,1)}, vec4(.5,.5,0,1), null, 10)
+	mainCamera.attachChildToSelf(playerLight, "relative")
 	//mainCamera.renderEngine = true
 	generateMaze_()
 	var tmp = getRect(vec3(0, 0, 0), vec3(100, 1, 100))
