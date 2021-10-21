b3baa284b762a6e4cd954924204af2747ef84954
Andy Herbert <andyman1222@users.noreply.github.com>
2021-10-21 17:02:35 -0400

    Update userCode.js

--- engine/ProgrammingAssignment6/userCode.js
+++ engine/ProgrammingAssignment6/userCode.js
@@ -27,7 +27,7 @@ function switchCamera() {
 
 
 function userTick(delta, time) {
-	directLight.transform.rot = addRotation(directLight.transform.rot, eulerTOQuat(vec3(0,1,0),delta*.1))
+	directLight.transform.rot = addRotation(directLight.transform.rot, eulerToQuat(vec3(0,1,0),delta*.1))
 	for (var i = 0; i < keys.length; i++)
 		if (keys[i]) {
 			if (mainCamera.enabled) {
