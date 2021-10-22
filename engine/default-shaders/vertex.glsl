c737721bb739104e21ca8308156d2c2234ff5309
Andy Herbert <andyman1222@users.noreply.github.com>
2021-10-22 00:58:02 -0400

    Update vertex.glsl

--- engine/default-shaders/vertex.glsl
+++ engine/default-shaders/vertex.glsl
@@ -49,6 +49,7 @@ void main(void) {
     matProp2 = inMatProp2;
     matProp3 = inMatProp3;
     matProp4 = inMatProp4;
+    matProp5 = inMatProp5;
     matIndex = inMatIndex;
     position = (vec4(1.,1.,-1., 1.)*coordinates).xyz;
     //normal = normalize((normalMatrix*vec4(inNormal, 0.0)).xyz);
