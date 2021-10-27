96e55eca99f7df5fe047765010d253b3967cd2f0
Andy Herbert <andyman1222@users.noreply.github.com>
2021-10-27 02:49:02 -0400

    Update fragment.glsl

--- engine/default-shaders/fragment.glsl
+++ engine/default-shaders/fragment.glsl
@@ -49,7 +49,7 @@ vec2 parallax(vec2 TexCoord, vec3 viewDir, float minLayers, float maxLayers, flo
 	float maxl = 32.;
 	if(maxLayers>0.) maxl=maxLayers;
 
-	float hs = 1.
+	float hs = 1.;
 	if(heightScale>0.) hs=heightScale;
 
 	float nl = mix(maxl, minl, max(dot(vec3(0.0, 0.0, 1.0), viewDir), 0.0));
