87bc919c0b8eab57d4bbc38142b586e9b0d168b4
andyman1222 <andyman1222@users.noreply.github.com>
2021-10-27 13:39:20 -0400

    Update vertex.glsl

--- engine/default-shaders/vertex.glsl
+++ engine/default-shaders/vertex.glsl
@@ -34,10 +34,10 @@ out vec4 matProp[5];
 
 void main(void) {
     gl_Position = projMatrix * viewMatrix * coordinates;
-    vec3 T = normalize((normalMatrix*vec4(inTangent, 0.0)).xyz);
-    vec3 B = normalize((normalMatrix*vec4(inBiTangent, 0.0)).xyz);
+    vec3 T = normalize((viewmatrix*vec4(inTangent, 0.0)).xyz);
+    vec3 B = normalize((viewmatrix*vec4(inBiTangent, 0.0)).xyz);
     // aNormal can be calculated from aTangent and a Bitangent
-    vec3 N = normalize((normalMatrix*vec4(inNormal, 0.0)).xyz);
+    vec3 N = normalize((viewmatrix*vec4(inNormal, 0.0)).xyz);
 
     TBN = transpose(mat3(T, B, N));
 
