1b10ce30a4001f464efb6ff4d040b00edcb7d0d2
Andy Herbert <andyman1222@users.noreply.github.com>
2021-10-27 03:37:07 -0400

    Update maze.js

--- engine/ProgrammingAssignment7/maze.js
+++ engine/ProgrammingAssignment7/maze.js
@@ -225,7 +225,7 @@ draw_maze()
         p2 = vertex[Math.floor(perimeter[i].vertex2)];
         var tmp = _getRect(vec3(0,0,0), vec3(Math.abs(p2[0]-p1[0])/2+.5, 5, Math.abs(p2[1]-p1[1])/2+.5))
         walls.push(new _Object({pos: vec3((p2[0]+p1[0])/2, 5, (p2[1]+p1[1])/2), rot: Quaternion(0,1,0,0), scl: vec3(1,1,1)},
-        [{pointIndex: tmp.index, matIndex: [0], texCoords: tmp.texCoords, type: _gl.TRIANGLES, normals: tmp.normals, tangents: tmp.tangents, bitangents: tmp.bitangents, textureIndex: 0}],
+        [{pointIndex: tmp.index, matIndex: [2], texCoords: tmp.texCoords, type: _gl.TRIANGLES, normals: tmp.normals, tangents: tmp.tangents, bitangents: tmp.bitangents, textureIndex: 0}],
         tmp.points, [new _Material()], _Bounds._RECT, [new _ComplexTexture(_gl, ["images/Brick_Wall_015_COLOR.jpg", "images/Brick_Wall_015_DISP.png", "images/Brick_Wall_015_NORM.jpg", "images/Brick_Wall_015_OCC.jpg", "images/Brick_Wall_015_ROUGH.jpg"])]))
       }
     }
