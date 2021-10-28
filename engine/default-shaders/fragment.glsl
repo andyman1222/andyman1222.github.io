8716b0a7f2fec736bbce9bbe165cda5e9f94a5f4
Andy Herbert <andyman1222@users.noreply.github.com>
2021-10-28 00:20:01 -0400

    Update fragment.glsl

--- engine/default-shaders/fragment.glsl
+++ engine/default-shaders/fragment.glsl
@@ -248,19 +248,26 @@ vec4 standardMaterial(vec4 mp[6], vec3 norm, vec3 pos){
 
 //with parallax
 //There's some sort of error I have no idea about so for now it's disabled
-/*
+
 vec4 standardImageFull(vec4 mp[6], vec3 pos, vec2 tx, vec3 viewdir, float min, float max, float scale){
-	vec2 txp = parallax((tx*vec2(mp[5][0], mp[5][1]))+vec2(mp[5][2], mp[5][3]), viewdir, min, max, scale);
-	vec3 norm = normalize(texture(normalMap, txp).rgb*2.-1.);
+	vec2 txp = (tx*vec2(mp[5][0], mp[5][1]))+vec2(mp[5][2], mp[5][3]);
+	vec2 txCoords = parallax(txp, viewdir, min, max, scale);
+	vec3 norm = TBN * normalize(texture(normalMap, txCoords).rgb*2.-1.);
 	sMat mat = getStandardMaterial(mp[4], norm, pos);
-	vec4 txDiff = texture(diffuseMap, txp);
-	vec4 txSpec = texture(roughnessMap, txp);
+	vec4 txDiff = texture(diffuseMap, txCoords); //AO map
+	//vec4 txDiff = vec4(1.,1.,1.,1.);
+	vec4 txSpec = texture(roughnessMap, txCoords)*mp[2];
 	//vec4 txSpec = vec4(vec3(1.,1.,1.)-txRough.rgb,txRough.a);
-	vec4 txBase = texture(baseImage, txp);
-	vec4 tmp=vec4(((mat.ambient*mp[3]*mp[0]*txBase)+(mat.diffuse*mp[1]*mp[0]*txDiff*txBase)+(mat.specular*mp[2]*txSpec)).rgb,txBase.a*mat.ambient.a*mp[3].a*mp[0].a*mat.diffuse.a*mp[1].a*mp[0].a*mat.specular.a*mp[2].a);
+	//vec4 txSpec = vec4(1.,1.,1.,1.);
+	vec4 txBase = texture(baseImage, txCoords)*mp[0];
+	vec4 amb = mat.ambient*mp[3]*txDiff;
+	vec4 dif = mat.diffuse*mp[1];
+	vec4 spe = mat.specular*txSpec;
+	vec4 tmp=vec4(((amb*txBase)+(dif*txBase)+(spe)).rgb,
+	mix(1., amb.a, length(amb))*mix(1., dif.a, length(dif))*mix(1., spe.a, length(spe))*txBase.a);
 	return vec4(max(tmp.r,0.),max(tmp.g,0.),max(tmp.b,0.),clamp(tmp.a,0.,1.));
 	//return tmp;
-}*/
+}
 
 //no parallax
 vec4 standardImage(vec4 mp[6], vec3 pos, vec2 tx){
