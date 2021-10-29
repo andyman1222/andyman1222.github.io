064ed52f27dc2f3b47d52488af6bd1b231b0b3ed
Andy Herbert <andyman1222@users.noreply.github.com>
2021-10-29 04:59:41 -0400

    Update fragment.glsl

--- engine/default-shaders/fragment.glsl
+++ engine/default-shaders/fragment.glsl
@@ -280,7 +280,7 @@ switch(matIndex){
 	break;
 
 	case 2: //parallaxed texture
-	txc = parallax(texCoord, -normalize(cameraPos-position), normal, matProp[4][1], matProp[4][2], matProp[4][3]);
+	txc = parallax(txc, -normalize(cameraPos-position), normal, matProp[4][1], matProp[4][2], matProp[4][3]);
 	//break;
 
 	case 3: //texture, no parallax
@@ -288,7 +288,7 @@ switch(matIndex){
 	break;
 
 	case 4: //unlit texture, parallax
-	txc = parallax(texCoord, -normalize(cameraPos-position), normal, matProp[4][1], matProp[4][2], matProp[4][3]);
+	txc = parallax(txc, -normalize(cameraPos-position), normal, matProp[4][1], matProp[4][2], matProp[4][3]);
 
 	case 5: //unlit texture, no parallax
 	fColor = texture(baseImage, txc) * matProp[0];
