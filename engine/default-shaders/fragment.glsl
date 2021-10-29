f7ce0c3a79c78cb6c8b2577de96c1743edff3cd2
Andy Herbert <andyman1222@users.noreply.github.com>
2021-10-28 23:42:31 -0400

    Update fragment.glsl

--- engine/default-shaders/fragment.glsl
+++ engine/default-shaders/fragment.glsl
@@ -281,7 +281,7 @@ switch(matIndex){
 	break;
 
 	case 2: //parallaxed texture
-	txc = parallax(txc, normalize((TBN*cp)-(TBN*position))), TBN*(normal*vec3(1,1,-1)), matProp[4][1], matProp[4][2], matProp[4][3]);
+	txc = parallax(txc, normalize((TBN*cp)-(TBN*position)), TBN*(normal*vec3(1,1,-1)), matProp[4][1], matProp[4][2], matProp[4][3]);
 	//break;
 
 	case 3: //texture, no parallax
