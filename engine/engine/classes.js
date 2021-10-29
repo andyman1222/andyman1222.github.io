ed7fb3c739b6a6a0c8c57de5e4179a13bd086f35
Andy Herbert <andyman1222@users.noreply.github.com>
2021-10-28 20:12:41 -0400

    Update classes.js

--- engine/engine/classes.js
+++ engine/engine/classes.js
@@ -324,7 +324,7 @@ class _Buffer {
 						this._gTarget.uniform1iv(this._lightAltNegativeArrayLoc[x], new Int32Array([l._handleNegativeAlt]))
 					case 2:
 						var t = l._getWorldTransform()
-						this._gTarget.uniform3fv(this._lightDirArrayLoc[x], flatten(forward(t.rot)))
+						this._gTarget.uniform3fv(this._lightDirArrayLoc[x], flatten(mult(vec3(1,1,-1), forward(t.rot))))
 						this._gTarget.uniform3fv(this._lightLocArrayLoc[x], flatten(t.pos))
 					case 1:
 						this._gTarget.uniform4fv(this._lightColorArrayLoc[x], flatten(l._color));
