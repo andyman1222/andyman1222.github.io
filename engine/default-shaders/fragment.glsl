e0b6178f8739bfbc09709c35d94cd86b04362008
Andy Herbert <andyman1222@users.noreply.github.com>
2021-10-28 22:34:39 -0400

    Update fragment.glsl

--- engine/default-shaders/fragment.glsl
+++ engine/default-shaders/fragment.glsl
@@ -59,7 +59,7 @@ vec2 parallax(vec2 tx, vec3 viewDir, vec3 norm, float minLayers, float maxLayers
 
 	float layerDepth=1./nl;
 	float currentLayerDepth=0.;
-	vec2 deltaTexCoord=viewDir.xy * hs / (-viewDir.z * nl);
+	vec2 deltaTexCoord=viewDir.xy * hs / (viewDir.z * nl);
 	vec2 currentTexCoords=tx;
 	
 	float currentDepthMapValue=texture(depthMap,currentTexCoords).r;
