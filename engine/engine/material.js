cd416605b97a25d37f635ba04874e0e5c4300a24
Andy Herbert <andyman1222@users.noreply.github.com>
2021-11-01 20:31:32 -0400

    Update material.js

--- engine/engine/material.js
+++ engine/engine/material.js
@@ -34,7 +34,7 @@ class _SolidColorNoLighting extends _Material {
 }
 
 class _ScaledTexMat extends _Material {
-    constructor(parallax=false, uScale = 1, vScale = 1, uDisp=0, vDisp=0, minLayers=10, maxLayers=32, heightScale=1, parameters = [vec4(1, 1, 1, 1), vec4(.5, .5, .5, 1), vec4(1, 1, 1, 1), vec4(1, 1, 1, 1), vec4(1, 10, 32, 1), vec4(1, 1, 0, 0)]){
+    constructor(parallax=false, uScale = 1, vScale = 1, uDisp=0, vDisp=0, minLayers=10, maxLayers=32, heightScale=0, parameters = [vec4(1, 1, 1, 1), vec4(.5, .5, .5, 1), vec4(1, 1, 1, 1), vec4(1, 1, 1, 1), vec4(1, 10, 32, 1), vec4(1, 1, 0, 0)]){
         super(0, [parameters[0], parameters[1], parameters[2], parameters[3], vec4(parameters[0], minLayers, maxLayers, heightScale), vec4(uScale, vScale, uDisp, vDisp)])
         if(parallax) this._index = 2;
         else this._index = 3;
@@ -42,7 +42,7 @@ class _ScaledTexMat extends _Material {
 }
 
 class _ScaledTexMatNoLight extends _ScaledTexMat {
-    constructor(parallax=false, uScale = 1, vScale = 1, uDisp=0, vDisp=0, minLayers=10, maxLayers=32, heightScale=1, parameters = [vec4(1, 1, 1, 1), vec4(.5, .5, .5, 1), vec4(1, 1, 1, 1), vec4(1, 1, 1, 1), vec4(1, 10, 32, 1), vec4(1, 1, 0, 0)]){
+    constructor(parallax=false, uScale = 1, vScale = 1, uDisp=0, vDisp=0, minLayers=10, maxLayers=32, heightScale=0, parameters = [vec4(1, 1, 1, 1), vec4(.5, .5, .5, 1), vec4(1, 1, 1, 1), vec4(1, 1, 1, 1), vec4(1, 10, 32, 1), vec4(1, 1, 0, 0)]){
         super(parallax, uScale, vScale, uDisp, vDisp, minLayers, maxLayers, heightScale, parameters)
         if(parallax) this._index = 4;
         else this._index = 5;
