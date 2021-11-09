150f72d3c926b2a90400784dcb361d6d2790c9c1
Andy Herbert <andyman1222@users.noreply.github.com>
2021-11-08 22:32:00 -0500

    f

--- engine/maze/userCode.js
+++ engine/maze/userCode.js
@@ -179,7 +179,8 @@ function init() {
 	new _ComplexTexture(_gl, ["images/Brick_wall_008_COLOR.jpg", "images/Brick_wall_008_NORM.jpg", "images/Brick_wall_008_DISP.png", "images/Brick_wall_008_OCC.jpg", "images/Brick_wall_008_SPEC.jpg"]),
 	new _ComplexTexture(_gl, ["images/Wood_Floor_010_basecolor.jpg", "images/Wood_Floor_010_normal.jpg", "images/Wood_Floor_010_height.png", "images/Wood_Floor_010_ambientOcclusion.jpg", "images/Wood_Floor_010_roughness.jpg"])]
 
-	mat = new _ScaledTexMat(true, .1, .1, 0, 0, 8, 32, 1, [vec4(1, 1, 1, 1), vec4(1, 1, 1, 1), vec4(1, 1, 1, 1), vec4(1, 1, 1, 1), vec4(10, 8, 32, .001), vec4(1, 1, 0, 0)])
+	mat = new _ScaledTexMat(true, .1, .1, 0, 0, 8, 32, .1, [vec4(1, 1, 1, 1), vec4(1, 1, 1, 1), vec4(1, 1, 1, 1), vec4(1, 1, 1, 1), vec4(10, 8, 32, .001), vec4(1, 1, 0, 0)])
+	mat2 = new _ScaledTexMat(true, .1, .1, 0, 0, 8, 32, .1, [vec4(2, 1, 1, 1), vec4(1, 1, 1, 1), vec4(1, 1, 1, 1), vec4(1, 1, 1, 1), vec4(10, 8, 32, .001), vec4(1, 1, 0, 0)])
 
 	altCamera = new _Camera(_bData, vec3(0, 20, 0), eulerToQuat(vec3(1, 0, 0), 90), vec3(1, 1, 1))
 	altCamera._enabled = false
@@ -222,7 +223,7 @@ function init() {
 	tmp = _getRect(vec3(0,0,0), vec3(0, 10, 10))
 	new _Object({pos: vec3(-10, 0, 10), rot: eulerToQuat(vec3(0,0,1), 0), scl: vec3(1, 1, 1)},
 	[{pointIndex: tmp.index, matIndex: [0], texCoords: tmp.texCoords, type: _gl.TRIANGLES, normals: tmp.normals, tangents: tmp.tangents, textureIndex: 0}],
-	tmp.points, [mat], _Bounds._RECT, [txes[1]])
+	tmp.points, [mat2], _Bounds._RECT, [txes[1]])
 }
 
 window.onload = function () {
