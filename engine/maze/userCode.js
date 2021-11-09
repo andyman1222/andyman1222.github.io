595c6484de9b8fe3f1530b8fbae2b3064dde0beb
Andy Herbert <andyman1222@users.noreply.github.com>
2021-11-08 22:04:01 -0500

    Update userCode.js

--- engine/maze/userCode.js
+++ engine/maze/userCode.js
@@ -179,7 +179,7 @@ function init() {
 	new _ComplexTexture(_gl, ["images/Brick_Wall_008_COLOR.jpg", "images/Brick_Wall_008_NORM.jpg", "images/Brick_Wall_008_DISP.png", "images/Brick_Wall_008_OCC.jpg", "images/Brick_Wall_008_SPEC.jpg"]),
 	new _ComplexTexture(_gl, ["images/Wood_Floor_010_basecolor.jpg", "images/Wood_Floor_010_normal.jpg", "images/Wood_Floor_010_height.png", "images/Wood_Floor_010_ambientOcclusion.jpg", "images/Wood_Floor_010_roughness.jpg"])]
 
-	mat = new _ScaledTexMat(true, .5, .5, 0, 0, 8, 32, 1, [vec4(1, 1, 1, 1), vec4(1, 1, 1, 1), vec4(1, 1, 1, 1), vec4(1, 1, 1, 1), vec4(10, 8, 32, 1), vec4(1, 1, 0, 0)])
+	mat = new _ScaledTexMat(true, .25, .25, 0, 0, 8, 32, 1, [vec4(1, 1, 1, 1), vec4(1, 1, 1, 1), vec4(1, 1, 1, 1), vec4(1, 1, 1, 1), vec4(10, 8, 32, 1), vec4(1, 1, 0, 0)])
 
 	altCamera = new _Camera(_bData, vec3(0, 20, 0), eulerToQuat(vec3(1, 0, 0), 90), vec3(1, 1, 1))
 	altCamera._enabled = false
