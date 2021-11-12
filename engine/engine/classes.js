4a4dafd95f1a6a575449df5f831c26ea8097f8eb
Andy Herbert <andyman1222@users.noreply.github.com>
2021-11-12 00:54:00 -0500

    Update classes.js

--- engine/engine/classes.js
+++ engine/engine/classes.js
@@ -763,12 +763,16 @@ class _Camera extends _Primitive {
 		//var rotMat = mult(mult(rotateZ(this._transform.rot[2]), rotateY(-(this._transform.rot[1] - 90))), rotateX(-this._transform.rot[0]))//this may look wrong, and it most definately is, but it works
 	}
 
-	_updateCameraView(fov = 90, aspect = -1, orthographic = false, range = [.1, 200000], targetBuf = this._bufs[0], width=this._bufs[0]._gTarget.canvas.clientWidth, height=this._bufs[0]._gTarget.canvas.clientHeight) {
+	_updateCameraView(fov = 90, aspect = -1, orthographic = false, range = [.1, 200000], targetBuf = this._bufs[0], width=null, height=null) {
+		var w = width
+		var h = height
+		if(w == null) w = targetBuf._gTarget.canvas.clientWidth;
+		if(h == null) h = targetBuf._gTarget.canvas.clientHeight;
 		this._fov = fov;
 		this._ortho = orthographic;
 		this._range = range;
 		if (aspect < 0)
-			this._aspect = width / height
+			this._aspect = w / h
 		else this._aspect = aspect;
 		targetBuf._setProjMatrix(this._getProjMat());
 	}
