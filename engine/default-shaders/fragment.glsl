e774d03c2751c08ee44d5872b96c06bd6240474e
Andy Herbert <andyman1222@users.noreply.github.com>
2021-11-14 16:13:50 -0500

    Update fragment.glsl

--- engine/default-shaders/fragment.glsl
+++ engine/default-shaders/fragment.glsl
@@ -4,7 +4,7 @@ precision mediump float;
 
 const int LIGHT_COUNT=64;
 const int MAT_PROP_COUNT=6;
-const int ATTENUATION_DROPOFF=.01;
+const float ATTENUATION_DROPOFF=.01;
 in vec2 texCoord;
 in mat3 TBN;
 
