9762f5ac2b1367be4773626f4ac2ba92b819ae6f
Andy Herbert <andyman1222@users.noreply.github.com>
2021-11-15 18:29:59 -0500

    Update userCode.js

--- engine/ProgrammingAssignment7/userCode.js
+++ engine/ProgrammingAssignment7/userCode.js
@@ -195,6 +195,6 @@ function init() {
 }
 
 window.onload = function () {
-	this._engineInit("gl-canvas", init, userTick, userKeyEvent, userMouseEvent, defaultVertex = "https://andyman1222.github.io/engine/default-shaders/vertex.glsl", defaultFragment = "https://andyman1222.github.io/engine/default-shaders/fragment.glsl",
-	defaultPostVertex="https://andyman1222.github.io/engine/default-shaders/postprocess-vertex.glsl", defaultPostFragment="postprocess-fragment.glsl")
+	this._engineInit("gl-canvas", init, userTick, userKeyEvent, userMouseEvent, "https://andyman1222.github.io/engine/default-shaders/vertex.glsl", "https://andyman1222.github.io/engine/default-shaders/fragment.glsl",
+	"https://andyman1222.github.io/engine/default-shaders/postprocess-vertex.glsl", "postprocess-fragment.glsl")
 }
