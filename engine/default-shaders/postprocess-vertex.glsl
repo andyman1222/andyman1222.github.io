df5a8d465dbb19a040c3cf9a8673bb581bf91306
Andy Herbert <andyman1222@users.noreply.github.com>
2021-11-16 04:31:07 -0500

    Update postprocess-vertex.glsl

--- engine/default-shaders/postprocess-vertex.glsl
+++ engine/default-shaders/postprocess-vertex.glsl
@@ -7,5 +7,5 @@ out vec2 texCoords;
 
 void main(void){
     gl_Position = vec4(inPointsL, 1);
-    texCoords = (inPointsL/vec2(2,2))+vec2(.5,.5);
+    texCoords = (inPointsL.xy/vec2(2,2))+vec2(.5,.5);
 }
