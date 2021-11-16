df90f89f0f42e04992fec8da1523ca5b759a0a5c
Andy Herbert <andyman1222@users.noreply.github.com>
2021-11-16 05:36:29 -0500

    Update userCode.js

--- engine/cake/userCode.js
+++ engine/cake/userCode.js
@@ -148,7 +148,7 @@ class flame{
 	}
 
 	constructor(){
-		var tmp = _getSphere(vec3(0,1,0), vec3(.5, 1, .5), 5, 3);
+		var tmp = _getSphere(vec3(0,1,0), vec3(.1, 1, .3), 5, 3);
 		var matArr = []
 		for(var x = 0; x < 5; x++)
 				matArr.push(1, 1, 0)
@@ -193,7 +193,7 @@ var candles = []
 var cake
 function init() {
 	_mainCamera._transform.pos = vec3(0, 5, -15)
-	new _AmbientLight(vec4(.9/5,.7/5,.5/5,1), null)
+	new _AmbientLight(vec4(.9/5,.9/5,.9/5,1), null)
 	var tmp = _getRect(vec3(0, 0, 0), vec3(100, 1, 100))
 	new _Object({ pos: vec3(0, 0, 0), rot: eulerToQuat(vec3(0, 0, 1), 0), scl: vec3(1, 1, 1) }, [
 		{ pointIndex: tmp.index, matIndex: 
@@ -203,7 +203,7 @@ function init() {
 			1, 1, 1, 1, 1, 1,
 			1, 1, 1, 1, 1, 1,
 			1, 1, 1, 1, 1, 1], texCoords: tmp.texCoords, type: _gl.TRIANGLES, normals: tmp.normals, tangents: tmp.tangents, textureIndex: -1}]
-		, tmp.points, [new _BasicMaterial(vec4(.5,.5,.5,1), .5, 3, 1, vec4(0,0,0,1), 30), new _Material(-1)], _Bounds._RECT)
+		, tmp.points, [new _BasicMaterial(vec4(.5,.3,.1,1), .5, 3, 1, vec4(0,0,0,1), 30), new _Material(-1)], _Bounds._RECT)
 
 	var c = _getCylinder(vec3(0,0,0),vec3(10, 4, 10), 50)
 	cake = new _Object({pos: vec3(0,1,0), rot: eulerToQuat(vec3(0,0,1),0), scl: vec3(1,1,1)}, [_DrawInfo(
