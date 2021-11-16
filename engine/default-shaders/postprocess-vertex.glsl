6bd8df7acbb69d2f3eb1e57e802980600ae9f7dd
Andy Herbert <andyman1222@users.noreply.github.com>
2021-11-16 04:30:14 -0500

    dasdasdasdasd

--- engine/default-shaders/postprocess-vertex.glsl
+++ engine/default-shaders/postprocess-vertex.glsl
@@ -2,10 +2,10 @@
 
 precision highp float;
 
-in vec2 inPointsL;
+in vec3 inPointsL;
 out vec2 texCoords;
 
 void main(void){
-    gl_Position = vec4(inPointsL, -1, 1);
+    gl_Position = vec4(inPointsL, 1);
     texCoords = (inPointsL/vec2(2,2))+vec2(.5,.5);
 }
