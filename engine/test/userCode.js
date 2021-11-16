ae8603ad3651486229338cc0c72f3851a9cd78fe
Andy Herbert <andyman1222@users.noreply.github.com>
2021-11-16 00:26:22 -0500

    Update userCode.js

--- engine/test/userCode.js
+++ engine/test/userCode.js
@@ -202,7 +202,7 @@ function init() {
 	var x = new _Object({pos: vec3(0, 1, 0), rot: eulerToQuat(vec3(0,0,1),0),scl: vec3(1,1,1)},[{pointIndex: s.index, matIndex: [0], texCoords: s.texCoords, 
 	type: _gl.TRIANGLES, normals: s.normals, tangents: s.tangents, textureIndex: -1}], s.points, [new _BasicMaterial()], _Bounds._RECT, [])
 	x._attachSelfToParent(t, {pos: "noChange", rot: "noChange", scl: "noChange"})
-	t._customTickFunc = function(d, t) {this._transform.rot = addRotation(this._transform.rot, eulerToQuat(vec3(0,1,0), d*.01))}.bind(t)
+	t._customTickFunc = function(d, t) {this._transform.rot = addRotation(this._transform.rot, eulerToQuat(vec3(0,0,1), d*.01))}.bind(t)
 
 	
 }
