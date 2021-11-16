d57efdfd289a6e993de887ef528ea83d4a6bfb61
Andy Herbert <andyman1222@users.noreply.github.com>
2021-11-16 05:30:38 -0500

    Revert "undoing drawelements modifications (they're in a separate branch- no noticable performance gains found in the end)"
    This reverts commit d8e1965a6a932309fcc1ee00ace6546943f7ad75.

--- engine/default-shaders/postprocess-vertex.glsl
+++ engine/default-shaders/postprocess-vertex.glsl
@@ -2,10 +2,10 @@
 
 precision highp float;
 
-in vec3 inPointsL;
+in vec2 inPointsL;
 out vec2 texCoords;
 
 void main(void){
-    gl_Position = vec4(inPointsL, 1);
-    texCoords = (inPointsL.xy/vec2(2,2))+vec2(.5,.5);
+    gl_Position = vec4(inPointsL, -1, 1);
+    texCoords = (inPointsL/vec2(2,2))+vec2(.5,.5);
 }
