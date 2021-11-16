08038240cf07367e924a535feb2a1d76e5ded916
Andy Herbert <andyman1222@users.noreply.github.com>
2021-11-15 20:05:44 -0500

    Update postprocess-fragment.glsl

--- engine/ProgrammingAssignment7/postprocess-fragment.glsl
+++ engine/ProgrammingAssignment7/postprocess-fragment.glsl
@@ -23,7 +23,7 @@ const float scale = 50.;
 
 void main(void){
     vec4 results;
-    fColor = texture(scene, mix(round(texCoords*vec2(scale, scale))/vec2(scale,scale),texCoords, length(texture(depth, round(texCoords*vec2(scale, scale))/vec2(scale,scale))));
+    fColor = texture(scene, mix(round(texCoords*vec2(scale, scale))/vec2(scale,scale),texCoords, length(texture(depth, round(texCoords*vec2(scale, scale))/vec2(scale,scale)))));
     /*//fColor = vec4(t.rgb, 1);
     if(d.b > minDepth)
         fColor = t;
