6d89b5a1ee3dbe43f069d5a8de48dc24429c0db4
andyman1222 <andyman1222@users.noreply.github.com>
2022-09-07 17:11:22 -0600

    Update postprocess-fragment.glsl

--- engine/ProgrammingAssignment7/postprocess-fragment.glsl
+++ engine/ProgrammingAssignment7/postprocess-fragment.glsl
@@ -25,7 +25,8 @@ const vec4 fogColor = vec4(1., 0., 0., 1.);
 void main(void){
     vec4 results;
     //fColor = texture(scene, mix(round(texCoords*vec2(scale, scale))/vec2(scale,scale),texCoords, length(texture(depth, round(texCoords*vec2(scale, scale))/vec2(scale,scale)))));
-    fColor = mix(fogColor, texture(scene, texCoords), clamp(((texture(depth, texCoords)))*((texture(position, texCoords).y)),0.,1.));
+    //fColor = mix(fogColor, texture(scene, texCoords), clamp(((texture(depth, texCoords)))*((texture(position, texCoords).y)),0.,1.));
+    fColor = mix(fogColor, texture(scene, texCoords), clamp(((texture(position, texCoords).y)),0.,1.));
     //fColor = texture(position, texCoords);
     /*//fColor = vec4(t.rgb, 1);
     if(d.b > minDepth)
