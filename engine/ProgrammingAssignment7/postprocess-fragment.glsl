e33be9dde23bae5134e2d787cbd968cd73fe12ce
andyman1222 <andyman1222@users.noreply.github.com>
2022-09-08 16:18:54 -0600

    Update postprocess-fragment.glsl

--- engine/ProgrammingAssignment7/postprocess-fragment.glsl
+++ engine/ProgrammingAssignment7/postprocess-fragment.glsl
@@ -21,15 +21,16 @@ const float minDepth = .1;
 const float dist = 3.;
 const float scale = 50.;
 const vec4 fogColor = vec4(.5, .5, .25, 1.);
-const float minViewDist = 10.;
-const float maxViewDist = 20.;
+const float minViewDist = 5.;
+const float maxViewDist = 10.;
 const float minViewHeight = 1.;
 const float maxViewHeight = 5.;
 
 void main(void){
     vec4 results;
     //fColor = texture(scene, mix(round(texCoords*vec2(scale, scale))/vec2(scale,scale),texCoords, length(texture(depth, round(texCoords*vec2(scale, scale))/vec2(scale,scale)))));
-    fColor = mix(texture(scene, texCoords), fogColor, 1.-clamp(((texture(depth, texCoords).r-minViewDist)/(maxViewDist-minViewDist))*clamp((texture(position, texCoords).y-minViewHeight)/(maxViewHeight-minViewHeight),0.,1.),0.,1.));
+    //fColor = mix(texture(scene, texCoords), fogColor, 1.-clamp(((texture(depth, texCoords).r-minViewDist)/(maxViewDist-minViewDist))*clamp((texture(position, texCoords).y-minViewHeight)/(maxViewHeight-minViewHeight),0.,1.),0.,1.));
+    fColor = texture(depth, texCoords);
     //fColor = mix(fogColor, texture(scene, texCoords), clamp(((texture(position, texCoords).y-5.)),0.,1.));
     //fColor=(texture(position,texCoords)-vec4(1.,1.,0.,0.))/vec4(20.,1.,20.,1.);
     //fColor = texture(position, texCoords);
