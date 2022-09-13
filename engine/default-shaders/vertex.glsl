3f70c54e172369321bfbb18ae164a577a35edae1
andyman1222 <andyman1222@users.noreply.github.com>
2022-09-13 14:56:06 -0600

    Update vertex.glsl

--- engine/default-shaders/vertex.glsl
+++ engine/default-shaders/vertex.glsl
@@ -63,7 +63,7 @@ void main(void) {
     //normal = tsMatrix*N;
     positionW = coordsW.xyz;
     vec4 tmp = (viewMatrix * coordsW * (vec4(1.,1.,1.,1.) / vec4(inCameraScale, 1.)));
-    positionL = vec4(tmp.r, tmp.g, length(inCameraPosW-positionW), tmp.a);
+    positionL = vec4(tmp.r, tmp.g, length(coordsW), tmp.a);
 
     positionT = TBN*(coordsW.xyz);
     positionVT = TBN * (viewMatrix * coordsW).xyz;
